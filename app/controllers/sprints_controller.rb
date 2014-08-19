4dee7adbb64dc2bf77b7508d13d3c752393656f2
Chris Ryan <cryan@redhat.com>
2014-08-19 11:27:04 +0200

    Added sprint methods, updated routes.

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,2 +1,8 @@
 class SprintsController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/index'
   root 'pages#index'
+  resources :sprints
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
