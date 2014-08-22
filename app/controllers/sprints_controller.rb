69e39dc5282d317b70319bf8446336238c1dd8c2
Chris Ryan <cryan@redhat.com>
2014-08-22 13:19:36 +0200

    Added validation for the Sprints form.

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -2,12 +2,18 @@ class SprintsController < ApplicationController
   def index
   end
   def new
+    #This is present to prevent 'nil' errors when the form is loaded on the 'new' action
+    @sprint = Sprint.new
   end
   def create
     @sprint = Sprint.new(sprint_params)
 
-    @sprint.save
-    redirect_to @sprint
+    #If the form is successfully saved, display the saved info. Else, render the error.
+    if @sprint.save
+      redirect_to @sprint
+    else
+      render 'new'
+    end
   end
   def show
     @sprint = Sprint.find(params[:id])
--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,3 +1,4 @@
 class Sprint < ActiveRecord::Base
   has_many :daily_scrum
+  validates :sprint_number, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
