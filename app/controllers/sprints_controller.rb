01d39d09bdc0726da2ea0d9c0bb6d7b83936c1db
Chris Ryan <cryan@redhat.com>
2014-08-26 14:49:48 +0200

    Added and edit and show all feature for sprints.

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,10 +1,13 @@
 class SprintsController < ApplicationController
   def index
+    @sprints = Sprint.all
   end
+
   def new
     #This is present to prevent 'nil' errors when the form is loaded on the 'new' action
     @sprint = Sprint.new
   end
+
   def create
     @sprint = Sprint.new(sprint_params)
 
@@ -15,8 +18,24 @@ class SprintsController < ApplicationController
       render 'new'
     end
   end
+
   def show
     @sprint = Sprint.find(params[:id])
+  end
+
+  def edit
+    @sprint = Sprint.find(params[:id])
+  end
+
+  def update
+    @sprint = Sprint.find(params[:id])
+
+    #If the form is successfully saved, display the saved info. Else, render the error.
+    if @sprint.update(sprint_params)
+      redirect_to @sprint
+    else
+      render 'edit'
+    end
   end
 
   #Strong Parameters, new in Rails 4. This whitelists the
