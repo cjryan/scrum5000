440897f7e84f0e59b7e70e510e9c622146cd62f8
Chris Ryan <cryan@redhat.com>
2014-09-12 15:57:45 +0200

    Fixed the app crashing issue, which occured when a user edited an existing scrum.

--- app/models/daily_scrum.rb
+++ app/models/daily_scrum.rb
@@ -1,6 +1,7 @@
 class DailyScrum < ActiveRecord::Base
   belongs_to :sprint
   belongs_to :user
+  validates :scrum_user, presence: true, numericality: { greater_than: 0 }
   validates :scrum_date, presence: true, uniqueness: { scope: :scrum_user }
   validates :sprint_id, presence: true
   validates :scrum_yesterday, presence: true
