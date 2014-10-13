088516c8dec166a9756a5896190ee10428d77921
Chris Ryan <cryan@redhat.com>
2014-10-13 16:34:28 +0200

    Updated the bot_portal controller with appropriate logic from the view.

--- app/controllers/bot_portal_controller.rb
+++ app/controllers/bot_portal_controller.rb
@@ -12,13 +12,22 @@ class BotPortalController < ApplicationController
     @users.each do |user|
       @unreported << user.id
     end
+
     #Find users who have not filed a bug report
-    #if not, pop off of the array
+    #if not, pop them off of the array
     @reported = DailyScrum.select("scrum_user").where("scrum_date"=>scrum_time)
     @reported.each do |report|
       if @unreported.include? report[:scrum_user]
         @unreported.delete(report[:scrum_user])
       end
     end
+
+    #Send a JSON response with those that did not fill out a scrum
+    laggards = {}
+    @unreported.each do |laggard|
+      User.find_by(:id=>laggard).email
+      laggards[laggard.to_s] = User.find_by(:id=>laggard).email
+    end
+    @response = JSON.generate(laggards)
   end
 end
