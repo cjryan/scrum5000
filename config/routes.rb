daebc0146719eb9803b5035fe51444eee07f0fa6
Chris Ryan <cryan@redhat.com>
2016-02-19 22:51:39 -0600

    Added route for bot filed scrums.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post 'daily_scrums/search_all_scrums' => 'daily_scrums#search_all_scrums'
   resources :daily_scrums
   get 'bot_portal/bot_checkpoint' => 'bot_portal#bot_checkpoint'
+  post 'bot_portal/bot_filed_scrum' => 'bot_portal#bot_filed_scrum'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
