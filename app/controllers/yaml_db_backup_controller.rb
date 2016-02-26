02a6c944b1b8d289b8b792c5fc3a6ccb14873668
Chris Ryan <cryan@redhat.com>
2016-02-25 18:08:15 -0600

    Added db dump serving via yaml support.

--- app/controllers/yaml_db_backup_controller.rb
+++ app/controllers/yaml_db_backup_controller.rb
@@ -1,4 +1,14 @@
 class YamlDbBackupController < ApplicationController
+  before_filter :authenticate_user!
+
   def render_yaml
+    send_file(
+      "#{ENV['OPENSHIFT_DATA_DIR']}/dumps/data.yml",
+      filename: "test.yml",
+      type: "text/plain"
+    )
+  end
+
+  def list_backups
   end
 end
