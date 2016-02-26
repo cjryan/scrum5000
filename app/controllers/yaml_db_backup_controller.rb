95a77b6c46842ee4bdde23aa3379c85d2eea2c2b
Chris Ryan <cryan@redhat.com>
2016-02-26 14:52:32 -0600

    Added support for HTTP Auth, to allow for remote backups.

--- app/controllers/yaml_db_backup_controller.rb
+++ app/controllers/yaml_db_backup_controller.rb
@@ -1,5 +1,15 @@
 class YamlDbBackupController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate
+
+  #Heavily borrowed from:
+  #https://coderwall.com/p/9nib6w/rails-devise-and-basic-http-auth-oh-my
+  def authenticate
+    authenticate_or_request_with_http_basic do |email, password|
+      user = User.where(email: email).first
+      !user.nil? && user.valid_password?(password)
+    end
+    warden.custom_failure! if performed?
+  end
 
   def render_yaml
     send_file(
